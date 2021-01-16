aa7e48c416388892dbd1b3d6a272ddfb18dd5abb
sergehould <sergehould@yahoo.ca>
2021-01-16 16:58:32 -0500

    Signed-off-by: sergehould <sergehould@yahoo.ca>

---
+++ Project1/Project1/main.cpp
@@ -0,0 +1,22 @@
+/****************************************************************************
+*
+*
+*	Author				Date			Version
+*	
+*****************************************************************************/
+
+#include <Windows.h> 
+#include <stdio.h>
+#include <string.h>
+#define HAVE_STRUCT_TIMESPEC  // for win32 only. Because TIMESPEC is re-defined inside pthread.h
+#include <pthread.h>
+
+
+           
+
+int main(int argc, char* argv[])
+{
+   
+}  
+
+
